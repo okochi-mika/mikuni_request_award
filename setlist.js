a71ae33c84ca7b2175fc2eb29c23218514f6e397
okochi-mika <hiyoko_m@icloud.com>
2026-04-19 10:42:11 +0900

    セトリを更新

--- setlist.js
+++ setlist.js
@@ -1,14 +1,22 @@
 // setlist.js の冒頭
 const setlistData = [
-    { title: "1. 君がいるから ", id: "vSEqeEtLMG4" }, // YouTube IDを調べて入れる
+    { title: "1. 君がいるから ", id: "npE8RBnBsAk" }, // YouTube IDを調べて入れる
     { title: "2. 君に吹く風", id: "iPkuvrQaJQg" },
-    { title: "3. 2000EXPRESS", id: "TMoOdbjcjGU" }, 
-    { title: "4. TRUE", id: "HEHvGgZsdl8" },
-    { title: "5. all the way", id: "ZjvQ6Vj_wuw" },
-    { title: "6. KOHAKU", id: "FZsIi1KH4xI" },
-    { title: "7. Life", id: "0mTnSzDFhyE" },
-    { title: "8. 枯れない花", id: "HKOy3b5ZSVQ" },
-    { title: "9. はじまり", id: "JjWwfUbOP0w" },
-    { title: "10. Alone", id: "KKYMYE1gyNI" },
-    { title: "11. 南風", id: "f8Nv-5wIlbg" }
+    { title: "3. 2000EXPRESS", id: "EHD0Tm-wji0" }, 
+    { title: "4. TRUE", id: "tHwMV_zfq2A" },
+    { title: "5. 枯れない花", id: "HKOy3b5ZSVQ" },
+    { title: "6. この場所が好きで", id: "tMR3hb4iR34" },
+    { title: "7. Alone", id: "JjWwfUbOP0w" },
+    { title: "8. 初恋の君", id: "vBfJBE0KqPc" },
+    { title: "9. キミノユメ", id: "HEHvGgZsdl8" },
+    { title: "10. それが、愛でしょう", id: "TMoOdbjcjGU" },
+    { title: "11. 39", id: "JyDdk_IBc_A" },
+    { title: "12. サヨウナラ! 20世紀の僕たち", id: "9u8OTofjYuI" },
+    { title: "13. さよならも言えなかった夏", id: "dgS2dJ_VE78" },
+    { title: "14. KOHAKU", id: "FZsIi1KH4xI" },
+    { title: "15. Life", id: "0mTnSzDFhyE" },
+    { title: "16. 南風", id: "f8Nv-5wIlbg" },
+    { title: "17. Naked", id: "06PuQc6pwII" },
+    { title: "18. イ〜じゃナイ!?", id: "2tIb-6kiNZI" },
+    { title: "19. Remember", id: "f8Nv-5wIlbg" }
 ];
